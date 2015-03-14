5962c790cff386b4d3618cf5ce3a27f768d3bc41
Luis Osa <luis.osa.gdc@gmail.com>
2015-03-14 20:40:31 +0100

    Implement sum of binomials modulo
    Also, include more contracts and checks in the primes module

--- main.rkt
+++ main.rkt
@@ -1,8 +1,120 @@
 #lang racket
 
-(define x 1)
+
+(require "primes.rkt")
 
 (module+ test
-  (require rackunit)
-  (check-eq? x 2)
-)
+  (require math)
+  (require rackunit))
+
+
+(define/contract (exact-division prime a)
+  (natural-number/c natural-number/c . -> . natural-number/c)
+  (define power 1)
+  (while (((modulo a power) . = . 0)
+          . and .
+          (integer? (a . / . (power . * . prime))))
+    (set! power (power . * . prime)))
+  power)
+
+
+(module+ test
+  (test-case
+    "Exact power division"
+    (check-equal? (exact-division 5 9) 1)
+    (check-equal? (exact-division 5 50) 25)
+    (check-equal? (exact-division 3 21) 3)
+    (check-equal? (exact-division 2 60) 4)
+    (check-equal? (exact-division 3 60) 3)
+    (check-equal? (exact-division 5 60) 5)
+    (check-exn exn:fail:contract? (lambda () (exact-division -1 21)))
+    (check-exn exn:fail:contract? (lambda () (exact-division 3.0 21)))))
+
+
+(define/contract (decompose-powers a factors)
+  (natural-number/c (vectorof positive?) . -> . (vectorof positive?))
+  (define exact-divisors
+    (for/vector ([factor (in-vector factors)])
+        (exact-division factor a)))
+  (define remaining
+    (a . / . (product exact-divisors)))
+  (if (integer? remaining)
+    (vector-append (make-vector 1 remaining) exact-divisors)
+    (vector-append (make-vector 1 1) exact-divisors)))
+
+
+(module+ test
+  (test-case
+    "Decompose powers"
+    (let* ([a 60] [k 4] [m 30]
+           [raw-factors (factorization m)]
+           [factors (limit-factors k raw-factors)])
+      (check-equal? raw-factors #(2 3 5))
+      (check-equal? factors #(2 3))
+      (check-equal? (decompose-powers a factors) #(5 4 3)))
+    (let* ([a 60] [k 6] [m 30]
+           [raw-factors (factorization m)]
+           [factors (limit-factors k raw-factors)])
+      (check-equal? raw-factors #(2 3 5))
+      (check-equal? factors raw-factors)
+      (check-equal? (decompose-powers a factors) #(1 4 3 5)))
+    (let* ([a 6] [k 6] [m 30]
+           [factors (limit-factors k (factorization m))])
+      (check-equal? (decompose-powers a factors) #(1 2 3 1)))
+    (check-exn exn:fail:contract?
+      (lambda () (decompose-powers -1 (factorization 21))))
+    (check-exn exn:fail:contract?
+      (lambda () (decompose-powers 60.0 (factorization 21))))
+    (check-exn exn:fail:contract?
+      (lambda () (decompose-powers 60 #(-2 3 5))))))
+
+
+(define (product sequence)
+  (for/product ([elem sequence]) elem))
+
+
+(define (_ vec idx)
+  (vector-ref vec idx))
+
+
+(define/contract (sum-binomials-modulo k N m)
+  (natural-number/c natural-number/c positive? . -> . natural-number/c)
+  (let* ([factors (limit-factors k (factorization m))]
+         [l (vector-length factors)]
+         [A 1] [B 1] [C 1]
+         [R (make-vector (vector-length factors) 1)]
+         [a 0] [b 0])
+    (for* ([j (in-range 1 k)]
+           [i (in-range l)])
+      (set! a (+ (- N j) 1)) ; N written 'n' in paper
+      (set! b j)
+      (define a-powers (decompose-powers a factors))
+      (define b-powers (decompose-powers b factors))
+      (define a* (vector-ref a-powers 0))
+      (define b* (vector-ref b-powers 0))
+      (vector-set! R i
+        ((R . _ . i) . * .  ((a-powers . _ . i) . / . (b-powers . _ . i))))
+      (set! A (modulo (* A a*) m))
+      (set! B (modulo (* B b*) m))
+      (set! C (+ (* C (vector-ref b-powers 0)) (modulo (* A (product R)) m))))
+    (/ C (modulo B m))))
+
+
+(module+ test
+  (test-case
+    "Sum of binomials modulo an integer"
+    (define epsilon 0.01)
+    (define (slow-sum-binomials-modulo k N m)
+      (for/sum ([i (in-range (add1 k))]) (modulo (binomial N i) m)))
+    (let* ([k 6] [N 6] [m 30]
+           [S (slow-sum-binomials-modulo k N m)])
+      (check-= (sum-binomials-modulo k N m) S epsilon))
+    (let* ([k 6] [N 6] [m 30]
+           [S (slow-sum-binomials-modulo k N m)])
+      (check-= (sum-binomials-modulo k N m) S epsilon))
+    (check-exn exn:fail:contract? (lambda () (sum-binomials-modulo -6 6 30)))
+    (check-exn exn:fail:contract? (lambda () (sum-binomials-modulo 6 -6 30)))
+    (check-exn exn:fail:contract? (lambda () (sum-binomials-modulo 6 6 -30)))
+    (check-exn exn:fail:contract? (lambda () (sum-binomials-modulo 6.0 6 30)))
+    (check-exn exn:fail:contract? (lambda () (sum-binomials-modulo 6 6.0 30)))
+    (check-exn exn:fail:contract? (lambda () (sum-binomials-modulo 6 6 30.0)))))
--- primes.rkt
+++ primes.rkt
@@ -1,4 +1,6 @@
 #lang racket
+(provide while factorization limit-factors)
+
 
 (define-syntax-rule (while condition body ...)
   (let loop ()
@@ -6,10 +8,9 @@
       body ...
       (loop))))
 
+
 (define/contract (erathostenes limit)
   ((>=/c 2) . -> . (vectorof positive?))
-  (when (limit . < . 0)
-    #())
   (define A (make-vector limit #t))
   (for ([i (in-range 2 (sqrt limit))])
     (when (vector-ref A i)
@@ -19,7 +20,9 @@
                #:when (vector-ref A idx))
     idx))
 
-(define (factorization n)
+
+(define/contract (factorization n)
+  ((>=/c 2) . -> . (vectorof positive?))
   (define primes (erathostenes n))
   (define factors #())
   (for ([trial (in-vector primes)])
@@ -28,16 +31,35 @@
       (set! n (/ n trial))))
   factors)
 
+
+(define/contract (limit-factors limit factors)
+  (natural-number/c (vectorof positive?) . -> . (vectorof positive?))
+  (vector-filter (lambda (x) (<= x limit)) factors))
+
+
 (module+ test
   (require rackunit)
+
   (test-case
     "Erathostenes sieve"
     (check-equal? (erathostenes 3) #(2))
     (check-equal? (erathostenes 10) #(2 3 5 7))
+    (check-equal? (vector-take-right (erathostenes 1000) 4) #(977 983 991 997))
     (check-exn exn:fail:contract? (lambda () (erathostenes -1)))
     (check-exn exn:fail:contract? (lambda () (erathostenes 0)))
     (check-exn exn:fail:contract? (lambda () (erathostenes 1))))
+
   (test-case
     "Prime factorization by trial division"
     (check-equal? (factorization 21) #(3 7))
-    (check-equal? (factorization 25) #(5 5))))
+    (check-equal? (factorization 25) #(5 5))
+    (check-exn exn:fail:contract? (lambda () (factorization -1)))
+    (check-exn exn:fail:contract? (lambda () (factorization 0)))
+    (check-exn exn:fail:contract? (lambda () (factorization 1))))
+
+  (test-case
+    "Limit factors to less or equal to a number"
+    (check-equal? (limit-factors 5 (factorization 21)) #(3))
+    (check-exn exn:fail:contract? (lambda () (limit-factors 5.0 #(3 7))))
+    (check-exn exn:fail:contract? (lambda () (limit-factors 5 '(3 7))))
+    (check-exn exn:fail:contract? (lambda () (limit-factors 5 #(-1 1))))))
